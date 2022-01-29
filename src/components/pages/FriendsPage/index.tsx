2e3b9aa29bdbd64bb05aa0f971000d25ce2ce768
Filip Johnsson <filip_johnsson@yahoo.se>
2022-01-30 00:32:28 +0100

    Cleaning

--- src/components/pages/FriendsPage/index.tsx
+++ src/components/pages/FriendsPage/index.tsx
@@ -1,23 +1,13 @@
-import { AppBar, Button, CircularProgress, Dialog, DialogActions, DialogContent, Divider, Fab, IconButton, List, ListItem, ListItemText, Stack, TextField, Toolbar, Typography } from "@mui/material";
-import React from "react";
 import { useState } from "react";
-import { latestRecipesRequest } from "../../../interface/requests";
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
-import CloseIcon from '@mui/icons-material/Close';
 import FriendsList from "../../../features/friends/FriendList";
 import UserSearchDialog from "../../../features/friends/UserSearch";
+import Stack from "@material-ui/core/Stack";
+import Fab from "@material-ui/core/Fab";
 
-interface Friends {
-    incomingRequests: any[],
-    outgoingRequests: any[],
-    currentFriends: any[],
-}
 
 const FriendsPage = () => {
-    const [showSearch, setShowSearch] = useState<boolean>(true)
-    const [friends, setFriends] = useState<Friends>({
-        incomingRequests: [], outgoingRequests: [], currentFriends: []
-    })
+    const [showSearch, setShowSearch] = useState<boolean>(false)
 
     return (
         <Stack>
